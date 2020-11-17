83dc84254c2392d8f8457f316dd626eabffb7a5a
Giselle <gisi_tina4@hotmail.com>
2020-11-17 12:14:37 +0100

    Test date

--- tests/Infraestructura/Files/LoggerTest.php
+++ tests/Infraestructura/Files/LoggerTest.php
@@ -4,15 +4,20 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
-
+use App\Infrastructure\Files\Logger;
 
 class LoggerTest extends TestCase
 {
 
-	public function test_example()
-	{
-		$this->assertEquals(1, 1);
+	public function test_logger_is_string()
+	{	
+		$logger= new Logger("date", "Holis", "usuario");
+
+		$result= $logger->getDate();
+
+		$this->assertIsString($result);
 	}
+
 }
 
 
